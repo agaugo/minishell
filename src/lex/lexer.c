613e11b726032aab366ef190f7e192b748de4d2b
TRSTN4 <trstnvb4@gmail.com>
2023-10-22 23:49:33 +0200

    added t_flag to new lexer.

--- src/lex/lexer.c
+++ src/lex/lexer.c
@@ -6,7 +6,7 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/21 13:42:34 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/10/22 19:21:00 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/10/22 23:48:39 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -143,52 +143,66 @@ static tokentype_t	parse_word_token(char **current)
 	return (T_WORD);
 }
 
+static tokentype_t	parse_flag_token(char **current)
+{
+	if (**current == '-' && isalnum(*(*current + 1)))
+	{
+		(*current)++;
+		while (isalnum(**current) || **current == '-')
+			(*current)++;
+		return (T_FLAG);
+	}
+	return (T_WORD); // Default return
+}
+
 token_t	*lexer(char *_userInput, char *envp[])
 {
-	token_t		*head;
-	token_t		*current_token;
-	char		*current;
-	tokentype_t	current_token_type;
+	token_t *head = NULL;
+	token_t *current_token = NULL;
+	char *current = _userInput;
 
-	head = NULL;
-	current_token = NULL;
-	current = _userInput;
 	while (*current != '\0')
 	{
 		char *start = current;
+		tokentype_t current_token_type = T_WORD;
+
 		if (is_whitespace(*current))
 		{
 			while (is_whitespace(*current))
 				current++;
 			continue;
 		}
-		current_token_type = parse_pipe_token(&current);
-		if (current_token_type == T_WORD)
+
+		if (*current == '|')
+			current_token_type = parse_pipe_token(&current);
+		else if (*current == '<' || *current == '>')
 			current_token_type = parse_redirect_token(&current);
-		if (current_token_type == T_WORD)
+		else if (*current == '-')
+			current_token_type = parse_flag_token(&current);
+		else if (*current == '\'' || *current == '\"')
 			current_token_type = parse_quote_token(&current);
-		if (current_token_type == T_WORD)
+		else if (*current == '$')
 			current_token_type = parse_special_token(&current);
-		if (current_token_type == T_WORD)
+		else
 			current_token_type = parse_word_token(&current);
 
-		// Create new token
-		if (current != start)
+		// Token creation and addition to the linked list
+		char *value = strndup(start, current - start);
+		token_t *new_token = init_new_token(value, current, current_token_type, envp);
+		new_token->envp = envp;
+		new_token->next = NULL;
+
+		if (!head)
 		{
-			token_t *new_token = init_new_token(start, current,
-												current_token_type, envp);
-			if (!head)
-			{
-				head = new_token;
-				current_token = head;
-			}
-			else
-			{
-				current_token->next = new_token;
-				current_token = new_token;
-			}
+			head = new_token;
+			current_token = head;
+		}
+		else
+		{
+			current_token->next = new_token;
+			current_token = new_token;
 		}
 	}
 	print_commands(head);
-	return (head);
+	return head;
 }
