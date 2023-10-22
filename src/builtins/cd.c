97231b927a2ec0c26cadd779b01300e8db8f3b3e
TRSTN4 <trstnvb4@gmail.com>
2023-10-23 00:38:08 +0200

    Added edgecase checks and improved functions.

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,87 +6,41 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/25 10:37:37 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/08/25 11:19:45 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/10/23 00:37:16 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-
-void cdHome(void)
+static void	cd_home(void)
 {
-    chdir(getenv("HOME"));
+	char	*home_path;
+
+	home_path = getenv("HOME");
+	if (home_path)
+		chdir(home_path);
+	else
+        handleError(-1, "Error: HOME environment variable not found.");
 }
 
-void cdAbsolutePath(char *_path)
+static void	cd_absolute_path(char *path)
 {
-    if (chdir(_path) != 0) {
-        handleError(127, "Error finding directory");
-    }
+	if (!path || chdir(path) != 0)
+        perror("Error: No such file or directory.");
 }
 
-void cdCommand(token_t *_token)
+void	ms_cd_command(token_t *token)
 {
-    char *_newPWD;
-    char *_direction;
+	char	*direction;
 
-    if (_token->next == NULL)
-        return (cdHome());
-    _newPWD = NULL;
-    _direction = _token->next->value;
-    if (access(_direction, F_OK) == 0)
-        return (cdAbsolutePath(_direction));
-//    else
-//        _newPWD = getRelativePath(_PWD, _direction);
-    cdAbsolutePath(_newPWD);
+	if (token->next == NULL)
+		cd_home();
+	else
+	{
+		direction = token->next->value;
+		if (access(direction, F_OK) == 0)
+			cd_absolute_path(direction);
+		else
+			cd_absolute_path(NULL);
+	}
 }
-
-
-/*
- ******IGNORE THIS, I COMPLICATED IT TOO MUCH ************
-char *getRelativePath(char *_PWD, char *_direction) {
-    char **_array;
-    int _index;
-
-    _index = 0;
-    _array = ft_split(_PWD, '/');
-    addDash(_array);
-    if (ft_strcmp(_direction, "..") == 0) //modularise this
-    {
-        while (_array[_index + 1] != NULL)
-            _index++;
-        _array[_index] = NULL;
-    }
-    return (glueArray(_array));
-}
-
-char *glueArray(char **_array)
-{
-    int _index;
-    char *_gluedString;
-
-    _index = 1;
-    _gluedString = ft_strdup(_array[0]);
-    while(_array[_index] != NULL)
-    {
-        _gluedString = ft_strjoin(_gluedString, _array[_index]);
-        _index++;
-    }
-    return(_gluedString);
-}
-
-void addDash(char **_array)
-{
-    int _index;
-
-    _index = 0;
-    _array[0] = ft_strjoin("/", _array[_index]);
-    while(_array[_index] != NULL)
-    {
-        _array[_index] = ft_strjoin(_array[_index], "/");
-        _index++;
-    }
-}
-
-
-*/
--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,22 +6,26 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/25 10:37:37 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/08/25 12:24:07 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/10/23 00:23:30 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-//RESTORE TERMINAL TO DEFAULT
-int restoreTerminal(struct termios *_oldTermios) {
-    if (tcsetattr(0, TCSANOW, _oldTermios)!= 0)
-        handleError(-1, "tcsetattr: Error restoring terminal");
-    return (0);
+static int	restore_terminal_to_default(struct termios *old_termios)
+{
+	if (tcsetattr(0, TCSANOW, old_termios) != 0)
+	{
+		perror("Error: Failed tcsetattr restoring terminal.");
+		return (-1);
+	}
+	return (0);
 }
 
-// EXIT SHELL
-void exitShell(struct termios *_oldTermios) {
-    printf("//exit//\n");
-    restoreTerminal(_oldTermios);
-    exit(0);
+void	ms_exit_shell(struct termios *old_termios)
+{
+	printf("//exit//\n");
+	if (restore_terminal_to_default(old_termios) == -1)
+		exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }
