02c07ad3938abc40f8b80d6a60f999fb25ad85ba
TRSTN4 <trstnvb4@gmail.com>
2023-10-22 19:24:02 +0200

    refactor lexer.

--- src/lex/lexer.c
+++ src/lex/lexer.c
@@ -6,153 +6,189 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/21 13:42:34 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/10/19 15:38:36 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/10/22 19:21:00 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void print_commands(token_t *head) {
-    token_t *current_token = head;
-
-    printf("----------- lexer debug -----------------------------------------------\n");
-    int i = 0;
-    while (current_token) {
-        printf("Token %d: %s, Type: %d Validity: %d\n", i, current_token->value, current_token->type, validateToken(current_token));
-        current_token = current_token->next;
-        i++;
-    }
-    printf("-----------------------------------------------------------------------\n");
+static int		is_whitespace(char c)
+{
+	return (c == ' ' || c == '\t');
 }
 
-token_t *create_token(char *value, tokentype_t type) {
-	token_t *token = malloc(sizeof(token_t));
-	token->value = value;
-	token->type = type;
-	return token;
+static token_t	*init_new_token(char *start, char *current, tokentype_t type,
+								char *envp[])
+{
+	token_t	*new_token;
+
+	new_token = malloc(sizeof(token_t));
+	if (!new_token)
+		return (NULL);
+	new_token->value = strndup(start, current - start);
+	new_token->type = type;
+	new_token->envp = envp;
+	new_token->next = NULL;
+	return (new_token);
 }
 
-int is_whitespace(char c) {
-	return c == ' ' || c == '\t';
+void			print_commands(token_t *head)
+{
+	token_t	*current_token;
+	int		i;
+
+	current_token = head;
+	i = 0;
+	printf("----------- lexer debug --------------------------------------\n");
+	while (current_token)
+	{
+		printf("Token %d: %s, Type: %d Validity: %d\n", i,
+				current_token->value, current_token->type,
+				validateToken(current_token));
+		current_token = current_token->next;
+		i++;
+	}
+	printf("--------------------------------------------------------------\n");
 }
 
-token_t *lexer(char *_userInput, char *envp[]) {
-    token_t *head = NULL;
-    token_t *current_token = NULL;
-    char *current = _userInput;
+static tokentype_t	parse_pipe_token(char **current)
+{
+	if (**current == '|')
+	{
+		(*current)++;
+		return (T_PIPE);
+	}
+	return (T_WORD); // Default return
+}
 
-    while (*current != '\0') {
-        char *start = current;
-        tokentype_t current_token_type = T_WORD;
+static tokentype_t	parse_redirect_token(char **current)
+{
+	if (*(*current + 1) != '\0' && **current == '<') 
+	{
+		if (*(*current + 1) == '<')
+		{
+			*current += 2;
+			return (T_HEREDOC);
+		}
+		else
+		{
+			(*current)++;
+			return (T_REDIRECT_IN);
+		}
+	}
+	else if (**current == '>')
+	{
+		if (*(*current + 1) != '\0' && *(*current + 1) == '>')
+		{
+			*current += 2;
+			return (T_APPEND_OUT);
+		}
+		else
+		{
+			(*current)++;
+			return (T_REDIRECT_OUT);
+		}
+	}
+	return (T_WORD); // Default return
+}
 
-        if (is_whitespace(*current)) {
-            while (is_whitespace(*current)) current++;
-            continue;
-        }
+static tokentype_t	parse_quote_token(char **current)
+{
+	if (**current == '\'')
+	{
+		(*current)++;
+		while (**current != '\'' && **current != '\0')
+			(*current)++;
+		if (**current == '\'')
+			(*current)++;
+		return (T_SINGLE_QUOTE);
+	}
+	else if (**current == '\"')
+	{
+		(*current)++;
+		while (**current != '\"' && **current != '\0')
+			(*current)++;
+		if (**current == '\"')
+			(*current)++;
+		return (T_DOUBLE_QUOTE);
+	}
+	return (T_WORD); // Default return
+}
 
-        if (*current == '|') {
-            current_token_type = T_PIPE;
-            current++;
-        }
-			else if (*(current + 1) != '\0' && *current == '<')
-			{
-				if (*(current+1) == '<')
-				{
-					current_token_type = T_HEREDOC;
-					current += 2;
-				}
-				else
-				{
-					current_token_type = T_REDIRECT_IN;
-					current++;
-				}
-			}
-			else if (*current == '>')
-			{
-				if (*(current + 1) != '\0' && *(current+1) == '>')
-				{
-					current_token_type = T_APPEND_OUT;
-					current += 2;
-				}
-				else
-				{
-					current_token_type = T_REDIRECT_OUT;
-					current++;
-				}
-			}
-			else if (*current == '\'')
-			{
-				current_token_type = T_SINGLE_QUOTE;
+static tokentype_t	parse_special_token(char **current)
+{
+	if (**current == '$')
+	{
+		if (*(*current + 1) == '?')
+		{
+			*current += 2;
+			return (T_EXIT_STATUS);
+		}
+		else
+		{
+			(*current)++;
+			while (isalnum(**current) || **current == '_')
+				(*current)++;
+			return (T_ENV_VARIABLE);
+		}
+	}
+	return (T_WORD); // Default return
+}
+
+static tokentype_t	parse_word_token(char **current)
+{
+	while (isalnum(**current) || **current == '_' || **current == '/'
+		|| **current == '.' || **current == '-')
+		(*current)++;
+	return (T_WORD);
+}
+
+token_t	*lexer(char *_userInput, char *envp[])
+{
+	token_t		*head;
+	token_t		*current_token;
+	char		*current;
+	tokentype_t	current_token_type;
+
+	head = NULL;
+	current_token = NULL;
+	current = _userInput;
+	while (*current != '\0')
+	{
+		char *start = current;
+		if (is_whitespace(*current))
+		{
+			while (is_whitespace(*current))
 				current++;
-				while (*current != '\'' && *current != '\0')
-					current++;
-				if (*current == '\'')
-					current++;
-			}
-			else if (*current == '\"')
-			{
-				current_token_type = T_DOUBLE_QUOTE;
-				current++;
-				while (*current != '\"' && *current != '\0')
-					current++;
-				if (*current == '\"')
-					current++;
-			}
-			else if (*current == '$')
-			{
-				if (*(current+1) == '?')
-				{
-					current_token_type = T_EXIT_STATUS;
-					current += 2;
-				}
-				else
-				{
-					current_token_type = T_ENV_VARIABLE;
-					current++;
-					while(isalnum(*current) || *current == '_')
-						current++;
-				}
-			}
-			else if (*current == '-') {
-				if (isalnum(*(current + 1)))
-				{
-					current_token_type = T_FLAG;
-					current++;
-					while(isalnum(*current) || *current == '-') {
-						current++; 
-					}
-				}
-			}
-			else if (isalnum(*current) || *current == '_' || *current == '/' || *current == '.' || (*current == '-' && isalnum(*(current + 1)))) {
-				current_token_type = T_WORD;
-				while(isalnum(*current) || *current == '_' || *current == '/' || *current == '.' || *current == '-')
-					current++; 
-			}
-        else if (*current == '\n') {
-            current_token_type = T_NEWLINE;
-            current++;
-        } else {
-            while (*current != '\0' && !is_whitespace(*current) && *current != '|')
-                current++;
-        }
+			continue;
+		}
+		current_token_type = parse_pipe_token(&current);
+		if (current_token_type == T_WORD)
+			current_token_type = parse_redirect_token(&current);
+		if (current_token_type == T_WORD)
+			current_token_type = parse_quote_token(&current);
+		if (current_token_type == T_WORD)
+			current_token_type = parse_special_token(&current);
+		if (current_token_type == T_WORD)
+			current_token_type = parse_word_token(&current);
 
-        if (current != start || current_token_type == T_PIPE) {
-            char *value = strndup(start, current - start);
-            token_t *new_token = malloc(sizeof(token_t));
-            new_token->value = value;
-            new_token->type = current_token_type;
-            new_token->envp = envp;
-            new_token->next = NULL;
-
-            if (!head) {
-                head = new_token;
-                current_token = head;
-            } else {
-                current_token->next = new_token;
-                current_token = new_token;
-            }
-        }
-    }
-    print_commands(head);
-    return head;
+		// Create new token
+		if (current != start)
+		{
+			token_t *new_token = init_new_token(start, current,
+												current_token_type, envp);
+			if (!head)
+			{
+				head = new_token;
+				current_token = head;
+			}
+			else
+			{
+				current_token->next = new_token;
+				current_token = new_token;
+			}
+		}
+	}
+	print_commands(head);
+	return (head);
 }
