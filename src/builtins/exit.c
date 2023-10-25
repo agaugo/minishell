d819c233ad214e01ee64746f3c3258360eb0a42b
TRSTN4 <trstnvb4@gmail.com>
2023-10-25 15:15:19 +0200

    Removed termios.

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,26 +6,14 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/25 10:37:37 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/10/25 12:01:05 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/10/25 15:10:24 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int	restore_terminal_to_default(data_t data)
-{
-	if (tcsetattr(0, TCSANOW, &data.orig_termios) != 0)
-	{
-		perror("Error: Failed tcsetattr restoring terminal.");
-		return (-1);
-	}
-	return (0);
-}
-
-void	ms_exit_shell(data_t data)
+void	ms_exit_shell(void)
 {
 	printf("exit\n");
-	if (restore_terminal_to_default(data) == -1)
-		exit(EXIT_FAILURE);
 	exit(EXIT_SUCCESS);
 }
--- src/main/init.c
+++ src/main/init.c
@@ -6,24 +6,11 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/24 23:38:22 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/10/25 14:10:36 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/10/25 15:09:17 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-int ms_set_terminal_settings(data_t data)
-{
-	struct termios _newTermios;
-	
-	if (tcgetattr(0, &data.orig_termios) != 0)
-        ms_handle_error(-1, "tcgetattr");
-	_newTermios = * &data.orig_termios;
-	_newTermios.c_lflag &= ~ECHOCTL;
-	if (tcsetattr(0, TCSANOW, &_newTermios) != 0)
-		ms_handle_error(-1, "tcsetattr");
-	return (0);
-}
 
 char	**ms_clone_envp(char **envp)
 {
