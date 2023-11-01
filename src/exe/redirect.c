dd2f70433e3b1924a5db2a3d29c54bfe0975a57e
Hugo <hugoflohildesousa@gmail.com>
2023-11-01 15:48:17 +0100

    redirect in

--- src/exe/redirect.c
+++ src/exe/redirect.c
@@ -12,11 +12,33 @@
 
 #include "../../includes/minishell.h"
 
-//void  ms_redirect_in(data_t data)
-//{
-//    if (!data.tokens)
-//        perror("Syntax Error");
-//}
+void  ms_redirect_in(data_t *data)
+{
+    int fd;
+    token_t *token;
+
+    token = data->tokens;
+    while (token->type != T_REDIRECT_IN)
+        token = token->next;
+    if (!token->next)
+    {
+        perror("Syntax Error");
+        return;
+    }
+    fd = open(token->next->value, O_RDONLY, 0666);
+    if (fd == -1)
+    {
+        perror("Error opening/creating file");
+        return ;
+    }
+    if (dup2(fd, 0) == -1)
+    {
+        perror("Error duplicating fd");
+        close(fd);
+    }
+    data->tokens->fd = fd;
+}
+
 
 void  ms_redirect_out(data_t *data)
 {
@@ -26,16 +48,19 @@ void  ms_redirect_out(data_t *data)
     token = data->tokens;
     while (token->type != T_REDIRECT_OUT)
         token = token->next;
-    if (!token->next) {
+    if (!token->next)
+    {
         perror("Syntax Error");
         return;
     }
     fd = open(token->next->value, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         perror("Error opening/creating file");
         return ;
     }
-    if (dup2(fd, 1) == -1) {
+    if (dup2(fd, 1) == -1)
+    {
         perror("Error duplicating fd");
         close(fd);
     }
@@ -44,8 +69,6 @@ void  ms_redirect_out(data_t *data)
 
 void ms_redirect(data_t *data)
 {
-//    if (data.tokens->next->type == T_REDIRECT_IN)
-//        ms_redirect_in(data);
     token_t *token;
 
     token = data->tokens;
@@ -53,8 +76,9 @@ void ms_redirect(data_t *data)
     {
         if (token->next->type == T_REDIRECT_OUT)
             ms_redirect_out(data);
-        if (token->next->type == T_REDIRECT_OUT)
-            ms_redirect_out(data);
+        if (token->next->type == T_REDIRECT_IN)
+            ms_redirect_in(data);
+
         token = token->next;
     }
 }
