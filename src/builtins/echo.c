396a39993b4cfc1c601c2c91908501615812000b
TRSTN4 <trstnvb4@gmail.com>
2023-12-08 13:06:17 +0100

    Fixed output issues.

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,25 +6,27 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/23 00:11:40 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/12/06 23:16:18 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/12/08 13:03:11 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void	ms_print_echo(token_t *token, char *str)
+#include "../../includes/minishell.h"
+
+static void ms_print_echo(token_t *token, char *str)
 {
-	if (!token->next || !token->next->value)
-		printf("\n");
-	else if (ft_strcmp(token->next->value, "-n") == 0)
-	{
-		if (token->next->next && token->next->next->value)
-		{
-			printf("%s", str);
-		}
-	}
-	else
-		printf("%s\n", str);
+    if (!token->next || !token->next->value)
+        printf("\n");
+    else if (ft_strcmp(token->next->value, "-n") == 0)
+    {
+        if (token->next->next && token->next->next->value)
+        {
+            printf("%s", str);
+        }
+    }
+    else
+        printf("%s\n", str);
 }
 
 void ms_echo_command(data_t *data, token_t *token)
@@ -32,16 +34,17 @@ void ms_echo_command(data_t *data, token_t *token)
     int flag_n = 0;
     int first_word = 1;
     int stdout_backup = dup(STDOUT_FILENO); // Backup stdout
-	int not_first = 0;
-	int has_redirect = 0;
+    int word_printed = 0;
+    int after_output_redirect = 0; // Flag for output redirection
 
     token = token->next; // Skip the echo token
 
     // Iterate through tokens
     while (token)
     {
-		if (token->type == T_PIPE)
-			break;
+        if (token->type == T_PIPE)
+            break;
+
         // Check for "-n" flag at the beginning
         if (first_word && ft_strcmp(token->value, "-n") == 0)
         {
@@ -68,40 +71,30 @@ void ms_echo_command(data_t *data, token_t *token)
                 dup2(fd, STDOUT_FILENO); // Redirect STDOUT to the file
                 close(fd);
                 token = token->next; // Move past the file name
+                after_output_redirect = 1; // Set the flag since we are after output redirection
                 continue; // Continue processing further tokens
             }
         }
+
         // Skip input redirection ("<") and its associated file name
-        else if (token->type == T_REDIRECT_IN)
+        if (token->type == T_REDIRECT_IN)
         {
-			has_redirect = 1;
-            token = token->next ? token->next->next : NULL; // Skip the "<" token and its following file name
+            token = token->next ? token->next->next : NULL;
             continue;
         }
 
         // Print tokens of type T_WORD
         if (token && token->type == T_WORD)
         {
-			if (not_first == 0)
-			{
-            	printf("%s", token->value);
-				not_first++;
-			}
-			else
-			{
-				if (has_redirect == 1)
-				{
-					has_redirect = 0;
-					printf(" %s", token->value);
-				}
-				else
-					printf("%s", token->value);
-			}
-
-            if (token->next && token->next->type == T_WORD)
+            if (word_printed > 0 || after_output_redirect)
             {
-                printf(" "); // Add a space between words
+                printf(" "); // Add a space if this is not the first word or after output redirection
             }
+
+            printf("%s", token->value);
+            word_printed++;
+
+            after_output_redirect = 0; // Reset the output redirect flag
         }
 
         token = token->next;
