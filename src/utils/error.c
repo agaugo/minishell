721825fe6c1eb435e96d16048b4dccbc85a5c3fa
TRSTN4 <trstnvb4@gmail.com>
2023-12-13 12:37:57 +0100

    Utils norminette/refactored.

--- src/utils/error.c
+++ src/utils/error.c
@@ -6,34 +6,28 @@
 /*   By: trstn4 <trstn4@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/21 19:24:57 by trstn4        #+#    #+#                 */
-/*   Updated: 2023/12/12 20:59:43 by trstn4        ########   odam.nl         */
+/*   Updated: 2023/12/13 12:24:20 by trstn4        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	ms_handle_error(int _exitCode, char *_errorMessage)
+void	ms_handle_error(int exit_code, char *message)
 {
-	perror(_errorMessage);
-	exit(_exitCode);
+	perror(message);
+	exit(exit_code);
 }
 
-void ms_free_2d_array(char **array)
+void	ms_free_2d_array(char **array)
 {
-    if (array == NULL)
-    {
-        return;
-    }
+	int	i;
 
-    for (int i = 0; array[i] != NULL; i++)
-    {
-        free(array[i]);
-    }
-
-    free(array);
-}
-
-void debug(char *output)
-{
-	printf("Content: %s\n", output);
+	if (array == NULL)
+		return ;
+	while (array[i] != NULL)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
 }
